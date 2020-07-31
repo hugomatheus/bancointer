b36dca13b3f33285834c7ea9e9846f6432fd7bdf
Hugo Matheus <hg.targino@gmail.com>
2020-07-31 10:24:09 -0300

    Deixando o projeto inicial igual o da aula

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import { ThemeProvider } from 'styled-components';
 
 import theme from './styles/theme';
 import GlobalStyles from './styles/global';
+import Header from './components/Header';
 
 const App: React.FC = () => (
   <ThemeProvider theme={theme}>
-    <div>Hello World</div>
+    <Header />
     <GlobalStyles />
   </ThemeProvider>
 );
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+    display:flex;
+    width: 100%;
+    height: 10.4rem;
+    background: ${({theme}) => theme.colors.background};
+`;
+
